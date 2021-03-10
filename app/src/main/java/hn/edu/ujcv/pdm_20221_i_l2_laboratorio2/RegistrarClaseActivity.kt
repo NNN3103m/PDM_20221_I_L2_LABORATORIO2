87e7abcd94d181a8af943bd510817e7b6266f5b0
NSTR3103 <nnn3103m@gmail.com>
2021-03-10 17:41:52 -0600

    Creacion de las activities principales

---
+++ app/src/main/java/hn/edu/ujcv/pdm_20221_i_l2_laboratorio2/RegistrarClaseActivity.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_20221_i_l2_laboratorio2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrarClaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrar_clase)
+    }
+}

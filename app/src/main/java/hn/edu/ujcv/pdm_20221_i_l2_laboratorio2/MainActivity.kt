3102ea5bc0139cdc550e44a5e8cca0a1f31aa419
NSTR3103 <nnn3103m@gmail.com>
2021-03-10 14:17:16 -0600

    Initial commit, start project

---
+++ app/src/main/java/hn/edu/ujcv/pdm_20221_i_l2_laboratorio2/MainActivity.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_20221_i_l2_laboratorio2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
